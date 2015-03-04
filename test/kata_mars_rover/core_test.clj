4b10c5e19f6340cf800535a324380bc83aaa7744
alvaro garcia <derecerca@gmail.com>
2015-03-04 20:44:33 +0100

    red: the rover should forward

--- test/kata_mars_rover/core_test.clj
+++ test/kata_mars_rover/core_test.clj
@@ -1,16 +1,10 @@
 (ns kata-mars-rover.core-test
-  (:require [clojure.test :refer :all]
+  (:require
             [kata-mars-rover.core :refer :all])
   (:use [midje.sweet]))
 
 (defn accept [position movements]
   position)
-
-(deftest mars-rover
-  (testing "the rover should not move on an empty movements"
-    (let [position {:x 0 :y 0}
-          movements ""]
-    (is (= position (accept position movements))))))
 
 (facts
  "about midje"
@@ -24,4 +18,14 @@
   "the rover should not move on an empty movements"
   (let [position {:x 0 :y 0}
           movements ""]
-    (accept position movements) => position)))
+    (accept position movements) => position))
+
+  (fact
+  "the rover should forward"
+  (let [position {:x 0 :y 0}
+        expected {:x 0 :y 1}
+          movements "f"]
+    (accept position movements) => expected))
+
+
+ )
